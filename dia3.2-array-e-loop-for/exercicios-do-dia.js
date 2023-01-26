8dcb53f4e2ef4c9e53d7b198e4502e0885a57ee4
Jean <trabalhecomjean@outlook.com.br>
2023-01-25 23:22:33 -0300

    exercicios do dia, aquecendo

---
+++ dia3.2-array-e-loop-for/exercicios-do-dia.js
@@ -0,0 +1,19 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// PERCORRA O ARRAY IMPRIMINDO TODOS OS VALORS NELE CONTTIDOS
+// COM A FUNÇÃO CONSOLE.LOG
+// for(let index = 0; index < numbers.length; index +=1){
+//     console.log(numbers[index]);
+// } FEITO E CHECADO
+// Some todos os valores contidos no array e imprima o resultado
+let somaTotal = 0
+for(let index = 0; index < numbers.length; index +=1){
+    somaTotal += numbers[index];
+}
+console.log(somaTotal)
+
+// Calcule e imprima a média aritmética dos valores contiidos no array
+// *A média aritméticaé o resultado da soma de todos os elementos dividido pelo
+// número total de elementos.
+// for(let index = 0; index < numbers.length; index +=1){
+//     console.log(index);
+// } * descobrindo quantos elementos (10)
